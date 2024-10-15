27f74c6d378665245de8df372eb4c284dfa9e8be
osama ahmed <osamagamil2070@gmail.com>
2024-10-15 19:47:50 +0300

    init

---
+++ lib/features/sports/presentation/screens/choose_your_game_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChooseYourGameScreen extends StatelessWidget {
+  const ChooseYourGameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Choose Your Game')),
+    );
+  }
+}
